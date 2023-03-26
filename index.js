7b1b664321b67f47388dd3aa22cccde0ac181282
Isaac Rutledge-Wilson <devpreneurpro@proton.me>
2023-03-26 15:46:26 -0700

    testpass

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+// Create an array called "cats" with initial values
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Array functions
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
